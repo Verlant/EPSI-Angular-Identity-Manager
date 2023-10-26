d0f88b0c60bec9d5ecb3428175fe1e149d3862ae
Antoine Verlyck <antoine.verlyck@gmail.com>
2023-10-26 20:53:52 +0200

    fin TD 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from "@angular/material/input";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 
 @NgModule({
   declarations: [
@@ -30,7 +32,9 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatInputModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ldap-list/ldap-list.component.ts
+++ src/app/ldap-list/ldap-list.component.ts
@@ -3,6 +3,7 @@ import {MatTableDataSource} from "@angular/material/table";
 import {UserLdap} from "../models/user-ldap";
 import {MatPaginator} from "@angular/material/paginator";
 import {LDAP_USERS} from "../models/ldap-mock-data";
+import {MatSlideToggleChange} from "@angular/material/slide-toggle";
 
 @Component({
   selector: 'app-ldap-list',
@@ -10,6 +11,7 @@ import {LDAP_USERS} from "../models/ldap-mock-data";
   styleUrls: ['./ldap-list.component.css']
 })
 export class LdapListComponent implements OnInit {
+  unactiveSelected: boolean;
   displayedColumns: string[] = ['nomComplet', 'mail', 'employeNumero'];
   dataSource = new MatTableDataSource<UserLdap>([]);
 
@@ -17,13 +19,40 @@ export class LdapListComponent implements OnInit {
 
   constructor() {
     this.paginator = null;
+    this.unactiveSelected = false;
   }
   ngOnInit(): void {
+    // console.log('Values on ngOnInit():');
     this.dataSource.paginator = this.paginator;
+    this.dataSource.filterPredicate = (data: UserLdap, filter: string) => this.filterPredicate(data, filter);
     this.getUsers();
+    // console.log('Mat Paginator:', this.paginator);
   }
 
+  filterPredicate(data: UserLdap, filter: string): boolean {
+    return !filter || data.nomComplet.toLowerCase().startsWith(filter);
+  }
+
+  applyFilter($event: KeyboardEvent): void {
+    const filterValue = ($event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  // ngAfterViewInit(): void {
+  //   console.log('Values on ngAfterViewInit():');
+  //   console.log('Mat Paginator:', this.paginator);
+  // }
+
   private getUsers(): void {
-    this.dataSource.data = LDAP_USERS;
+    if (this.unactiveSelected) {
+    this.dataSource.data = LDAP_USERS.filter(user => !user.active);
+    } else {
+      this.dataSource.data = LDAP_USERS;
+    }
+  }
+
+  unactiveChange($event: MatSlideToggleChange): void {
+    this.unactiveSelected = $event.checked;
+    this.getUsers();
   }
 }
