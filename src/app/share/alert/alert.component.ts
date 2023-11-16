ab4eb1a7a152884a11fd5c59c9245dbdec651ffa
Antoine Verlyck <a.verlyck@2s-agency.fr>
2023-11-16 13:01:06 +0100

    ajout du component alert, TD4 page 12 -> a suivre : 'Le composant LdapEditComponent'

--- src/app/share/alert/alert.component.ts
+++ src/app/share/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-alert',
@@ -6,5 +6,24 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./alert.component.css']
 })
 export class AlertComponent implements OnInit {
-  
+  @Input() type: string;
+  cssClass: string[] = ['alert', 'alert-dismissible', 'fade'];
+
+  constructor() {
+    this.type = 'info';
+  }
+
+  ngOnInit(): void {
+    let alert = 'alert-info';
+    switch (this.type) {
+      case 'success': alert = 'alert-success'; break;
+      case 'danger': alert = 'alert-danger'; break;
+      case 'warning': alert = 'alert-warning'; break;
+    }
+    this.cssClass.push(alert)
+  }
+
+  removeAlert(): void {
+    this.cssClass = ['alert-hide'];
+  }
 }
